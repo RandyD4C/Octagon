ed510b3f9c6157c62717101334ebc76d53b15661
Randy <chongyongxuan2000@gmail.com>
2026-04-07 17:01:28 +0800

    Added Google Tag for Analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,18 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-5LLZ85B1H4"></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-5LLZ85B1H4');
+`,
+                    }}
+                />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Poppins:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
